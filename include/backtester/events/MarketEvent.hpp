385debabb8b7bc036122004caca0581e94cb00e9
egogoboy <egor2005sudakov@gmail.com>
2026-07-06 09:56:13 +0300

    feat: implement MarketEvent

---
+++ include/backtester/events/MarketEvent.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "backtester/enums/EventType.hpp"
+#include "backtester/events/Event.hpp"
+#include "backtester/models/MarketData.hpp"
+
+class MarketEvent : public Event {
+  public:
+    MarketEvent(const MarketData &data) : data_{data} {}
+
+    EventType get_type() const override {
+        return EventType::MARKET;
+    }
+
+    MarketData get_data() const {
+        return data_;
+    }
+
+  private:
+    MarketData data_;
+};
