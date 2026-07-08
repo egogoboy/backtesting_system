4db84b419e30b2d43c4a953162391e34e3d61636
egogoboy <egor2005sudakov@gmail.com>
2026-07-08 12:08:25 +0300

    feat: implement Strategy base class

---
+++ include/backtester/strategy/Strategy.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "backtester/core/EventQueue.hpp"
+#include "backtester/events/MarketEvent.hpp"
+#include "backtester/events/SignalEvent.hpp"
+#include "backtester/models/Signal.hpp"
+
+class Engine;
+
+class Strategy {
+  public:
+    virtual void on_market(const MarketEvent &event) = 0;
+
+    Strategy(const Strategy &other) = default;
+    Strategy(Strategy &&other) = default;
+    Strategy &operator=(const Strategy &other) = default;
+    Strategy &operator=(Strategy &&other) = default;
+    virtual ~Strategy() = default;
+
+  protected:
+    void send_signal(const Signal &signal) {
+        event_queue_->push(std::make_shared<SignalEvent>(signal));
+    }
+
+  private:
+    friend Engine;
+
+    void set_event_queue(const EventQueue &queue) {
+        event_queue_ = std::make_shared<EventQueue>(queue);
+    }
+
+    std::shared_ptr<EventQueue> event_queue_;
+};
