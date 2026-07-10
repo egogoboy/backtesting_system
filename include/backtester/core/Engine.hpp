682bbc2c07d5adc955e15eb669f9d9daf36d805d
egogoboy <egor2005sudakov@gmail.com>
2026-07-10 14:03:40 +0300

    fix/feat(Engine): change shared_ptr<Strategy> to reference_wrapper<Strategy>, add reference_wrapper<DataFeed> member and add data_feed to the run() function and constructor

--- include/backtester/core/Engine.hpp
+++ include/backtester/core/Engine.hpp
@@ -1,20 +1,24 @@
 #pragma once
 
 #include "backtester/core/EventQueue.hpp"
+#include "backtester/data_feed/DataFeed.hpp"
 #include "backtester/strategy/Strategy.hpp"
 #include <chrono>
+#include <functional>
 
 class Engine {
   public:
-    Engine(Strategy &strategy)
-        : start_time_{std::chrono::system_clock::now()}, strategy_{strategy.shared_from_this()} {
-        strategy_->set_event_queue(event_queue_);
+    Engine(Strategy &strategy, DataFeed &data_feed)
+        : start_time_{std::chrono::system_clock::now()}, strategy_{strategy},
+          data_feed_(data_feed) {
+        strategy_.get().set_event_queue(event_queue_);
     }
 
     void run();
 
   private:
     std::chrono::system_clock::time_point start_time_;
-    std::shared_ptr<Strategy> strategy_;
+    std::reference_wrapper<Strategy> strategy_;
+    std::reference_wrapper<DataFeed> data_feed_;
     EventQueue event_queue_;
 };
--- src/core/Engine.cpp
+++ src/core/Engine.cpp
@@ -3,23 +3,26 @@
 #include <memory>
 
 void Engine::run() {
-    while (!event_queue_.empty()) {
-        std::shared_ptr<Event> event = event_queue_.front();
-        event_queue_.pop();
+    while (data_feed_.get().has_next()) {
+        event_queue_.push(std::make_shared<MarketEvent>(data_feed_.get().get_next_market_event()));
+        while (!event_queue_.empty()) {
+            std::shared_ptr<Event> event = event_queue_.front();
+            event_queue_.pop();
 
-        switch (event->get_type()) {
-        case EventType::MARKET:
-            strategy_->on_market(*std::static_pointer_cast<MarketEvent>(event));
-            break;
+            switch (event->get_type()) {
+            case EventType::MARKET:
+                strategy_.get().on_market(*std::static_pointer_cast<MarketEvent>(event));
+                break;
 
-        case EventType::SIGNAL:
-            break;
+            case EventType::SIGNAL:
+                break;
 
-        case EventType::ORDER:
-            break;
+            case EventType::ORDER:
+                break;
 
-        case EventType::FILL:
-            break;
+            case EventType::FILL:
+                break;
+            }
         }
     }
 }
