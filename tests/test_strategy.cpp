efb6ec09215e1e1c44e91ba9ebe978132b20146a
egogoboy <egor2005sudakov@gmail.com>
2026-07-10 14:17:49 +0300

    feat(Strategy): write tests for strategy

---
+++ tests/test_strategy.cpp
@@ -0,0 +1,45 @@
+#include "backtester/enums/Direction.hpp"
+#include "backtester/enums/OrderType.hpp"
+#include "backtester/events/MarketEvent.hpp"
+#include "backtester/models/Signal.hpp"
+#include "backtester/strategy/Strategy.hpp"
+#include <gtest/gtest.h>
+
+TEST(Strategy, BuyOnMarketStrategy) {
+    class BuyOnMarketStrategy : public Strategy {
+      public:
+        void on_market(const MarketEvent &event) override {
+            Signal signal(event.get_data().get_symbol(), Direction::LONG, OrderType::MARKET,
+                          event.get_data().get_close());
+            this->signal = std::make_unique<Signal>(signal);
+        }
+
+        std::unique_ptr<Signal> signal = nullptr;
+    };
+
+    MarketData data("EURUSD", {1, 2, 3, 4}, 5);
+
+    MarketEvent event(data);
+    BuyOnMarketStrategy strategy;
+    strategy.on_market(event);
+
+    EXPECT_EQ(strategy.signal.get() != nullptr, true);
+    EXPECT_EQ(strategy.signal.get()->get_direction(), Direction::LONG);
+}
+
+TEST(Strategy, DoNothingStrategy) {
+    class DoNothingStrategy : public Strategy {
+      public:
+        void on_market(const MarketEvent &event) override {}
+
+        std::unique_ptr<Signal> signal = nullptr;
+    };
+
+    MarketData data("EURUSD", {1, 2, 3, 4}, 5);
+
+    MarketEvent event(data);
+    DoNothingStrategy strategy;
+    strategy.on_market(event);
+
+    EXPECT_EQ(strategy.signal.get() == nullptr, true);
+}
