f4e415b310898b78f7695e086b9a69d787df41a6
egogoboy <egor2005sudakov@gmail.com>
2026-07-15 10:19:45 +0300

    feat(event): add FillAction as a class member to the Fill Event

--- include/backtester/events/FillEvent.hpp
+++ include/backtester/events/FillEvent.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
 #include "backtester/enums/EventType.hpp"
+#include "backtester/enums/FillAction.hpp"
 #include "backtester/events/Event.hpp"
 #include "backtester/models/Position.hpp"
 #include <memory>
 
 class FillEvent : public Event {
   public:
-    FillEvent(std::shared_ptr<const Position> position) : data_{std::move(position)} {}
+    FillEvent(std::shared_ptr<const Position> position, FillAction action)
+        : data_{std::move(position)}, action_{action} {}
 
     EventType get_type() const override {
         return EventType::FILL;
@@ -17,6 +19,11 @@ class FillEvent : public Event {
         return data_;
     }
 
+    FillAction get_action() const {
+        return action_;
+    }
+
   private:
     std::shared_ptr<const Position> data_;
+    FillAction action_;
 };
