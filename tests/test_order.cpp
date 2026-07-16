baaa943002f11f246c0ba1db6bc002c448e8bbae
egogoboy <egor2005sudakov@gmail.com>
2026-07-16 11:21:24 +0300

    fixup! fix(Order): replace std::string symbol with Instrument instrument

--- tests/test_order.cpp
+++ tests/test_order.cpp
@@ -2,27 +2,28 @@
 #include "backtester/enums/OrderStatus.hpp"
 #include "backtester/enums/OrderType.hpp"
 #include "backtester/models/Order.hpp"
+#include "test_utils.hpp"
 #include <gtest/gtest.h>
 
 TEST(Order, Types) {
-    Order order = Order::make_market("EURUSD", Direction::LONG, 1);
+    Order order = Order::make_market(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 1);
     EXPECT_EQ(order.get_type(), OrderType::MARKET);
 
-    order = Order::make_limit("EURUSD", Direction::LONG, 1, 1);
+    order = Order::make_limit(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 1, 1);
     EXPECT_EQ(order.get_type(), OrderType::LIMIT);
 
-    order = Order::make_stop("EURUSD", Direction::LONG, 1, 1);
+    order = Order::make_stop(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 1, 1);
     EXPECT_EQ(order.get_type(), OrderType::STOP);
 }
 
 TEST(Order, PendingAfterCreation) {
-    Order order = Order::make_market("EURUSD", Direction::LONG, 1);
+    Order order = Order::make_market(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 1);
 
     EXPECT_EQ(order.get_status(), OrderStatus::PENDING);
 }
 
 TEST(Order, CancelAfterExecution) {
-    Order order = Order::make_market("EURUSD", Direction::LONG, 1);
+    Order order = Order::make_market(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 1);
     order.execute();
 
     EXPECT_EQ(order.cancel(), false);
@@ -30,9 +31,9 @@ TEST(Order, CancelAfterExecution) {
 }
 
 TEST(Order, Id) {
-    Order first_order = Order::make_market("EURUSD", Direction::LONG, 1);
+    Order first_order = Order::make_market(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 1);
     EXPECT_EQ(first_order.get_id(), 1);
 
-    Order second_order = Order::make_market("EURUSD", Direction::LONG, 1);
+    Order second_order = Order::make_market(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 1);
     EXPECT_EQ(second_order.get_id(), 2);
 }
