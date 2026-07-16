cca49bc249df26c996cf8a61588b7b25be312800
egogoboy <egor2005sudakov@gmail.com>
2026-07-16 11:17:48 +0300

    feat(enum): implement opposite(Direciton) method in Direction class to get the opposite direction

--- include/backtester/enums/Direction.hpp
+++ include/backtester/enums/Direction.hpp
@@ -3,3 +3,14 @@
 #include <cstdint>
 
 enum class Direction : std::uint8_t { LONG, SHORT };
+
+inline Direction opposite(Direction direction) {
+    switch (direction) {
+    case Direction::LONG:
+        return Direction::SHORT;
+        break;
+    case Direction::SHORT:
+        return Direction::LONG;
+        break;
+    }
+}
