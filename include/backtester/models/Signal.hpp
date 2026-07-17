35fb082552511343f5d10e77191120361cb89a53
egogoboy <egor2005sudakov@gmail.com>
2026-07-17 16:26:35 +0300

    fix(Signal): remove std::optional from stop_loss and take_profit

--- include/backtester/models/Signal.hpp
+++ include/backtester/models/Signal.hpp
@@ -7,22 +7,19 @@
 
 class Signal {
   public:
-    static Signal make_market(const Instrument &instrument, Direction direction,
-                              std::optional<double> stop_loss = std::nullopt,
-                              std::optional<double> take_profit = std::nullopt) {
+    static Signal make_market(const Instrument &instrument, Direction direction, double stop_loss,
+                              double take_profit) {
         return Signal(instrument, direction, OrderType::MARKET, std::nullopt, stop_loss,
                       take_profit);
     }
 
     static Signal make_limit(const Instrument &instrument, Direction direction, double entry_price,
-                             std::optional<double> stop_loss = std::nullopt,
-                             std::optional<double> take_profit = std::nullopt) {
+                             double stop_loss, double take_profit) {
         return Signal(instrument, direction, OrderType::LIMIT, entry_price, stop_loss, take_profit);
     }
 
     static Signal make_stop(const Instrument &instrument, Direction direction, double entry_price,
-                            std::optional<double> stop_loss = std::nullopt,
-                            std::optional<double> take_profit = std::nullopt) {
+                            double stop_loss, double take_profit) {
         return Signal(instrument, direction, OrderType::STOP, entry_price, stop_loss, take_profit);
     }
 
@@ -42,18 +39,17 @@ class Signal {
         return entry_price_;
     }
 
-    std::optional<double> get_stop_loss_price() const {
+    double get_stop_loss_price() const {
         return stop_loss_price_;
     }
 
-    std::optional<double> get_take_profit_price() const {
+    double get_take_profit_price() const {
         return take_profit_price_;
     }
 
   private:
     Signal(const Instrument &instrument, Direction direction, OrderType type,
-           std::optional<double> entry_price, std::optional<double> stop_loss,
-           std::optional<double> take_profit)
+           std::optional<double> entry_price, double stop_loss, double take_profit)
         : instrument_{instrument}, direction_{direction}, type_{type}, entry_price_{entry_price},
           stop_loss_price_{stop_loss}, take_profit_price_{take_profit} {}
 
@@ -61,6 +57,6 @@ class Signal {
     Direction direction_;
     OrderType type_;
     std::optional<double> entry_price_;
-    std::optional<double> stop_loss_price_;
-    std::optional<double> take_profit_price_;
+    double stop_loss_price_;
+    double take_profit_price_;
 };
