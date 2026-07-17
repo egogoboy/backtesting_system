d49a2683eaf9728cc3f5f6adbd3dd863f35f1e55
egogoboy <egor2005sudakov@gmail.com>
2026-07-17 17:18:39 +0300

    fixup! feat(Order): add take_profit and stop_loss into Order model

--- tests/test_order.cpp
+++ tests/test_order.cpp
@@ -6,24 +6,24 @@
 #include <gtest/gtest.h>
 
 TEST(Order, Types) {
-    Order order = Order::make_market(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 1);
+    Order order = Order::make_market(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 0.1, 1.172, 1.175);
     EXPECT_EQ(order.get_type(), OrderType::MARKET);
 
-    order = Order::make_limit(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 1, 1);
+    order = Order::make_limit(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 1, 0.1, 1.172, 1.175);
     EXPECT_EQ(order.get_type(), OrderType::LIMIT);
 
-    order = Order::make_stop(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 1, 1);
+    order = Order::make_stop(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 1, 0.1, 1.172, 1.175);
     EXPECT_EQ(order.get_type(), OrderType::STOP);
 }
 
 TEST(Order, PendingAfterCreation) {
-    Order order = Order::make_market(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 1);
+    Order order = Order::make_market(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 0.1, 1.172, 1.175);
 
     EXPECT_EQ(order.get_status(), OrderStatus::PENDING);
 }
 
 TEST(Order, CancelAfterExecution) {
-    Order order = Order::make_market(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 1);
+    Order order = Order::make_market(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 0.1, 1.172, 1.175);
     order.execute();
 
     EXPECT_EQ(order.cancel(), false);
@@ -31,9 +31,11 @@ TEST(Order, CancelAfterExecution) {
 }
 
 TEST(Order, Id) {
-    Order first_order = Order::make_market(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 1);
+    Order first_order =
+        Order::make_market(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 0.1, 1.172, 1.175);
     EXPECT_EQ(first_order.get_id(), 1);
 
-    Order second_order = Order::make_market(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 1);
+    Order second_order =
+        Order::make_market(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 0.1, 1.172, 1.175);
     EXPECT_EQ(second_order.get_id(), 2);
 }
